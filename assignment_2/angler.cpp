8230e62840d2f75ecfd5d32013e0e6c5912f34e2
Gulyás Simone <gulyassimone@gmail.com>
2020-04-12 20:58:34 +0200

    the code is finished

---
+++ assignment_2/angler.cpp
@@ -0,0 +1,53 @@
+#include "angler.h"
+#include <sstream>
+
+using namespace std;
+Angler::Angler(string filename)
+{
+    _x.open(filename);
+    if(_x.fail())
+        throw FILE_NAME_ERROR;
+}
+
+void Angler::next()
+{
+    _end=false;
+    _curr.isCatfish=false;
+    _curr.isNotCatfish=false;
+    string line;
+    getline(_x,line);
+    if (!_x.fail() && line!="")
+    {
+        istringstream in(line);
+
+        in >> _dx.anglerId >> _dx.contestId;
+        while( in >> _dx.fish >> _dx.sizeOfFish )
+        {
+
+            _curr.anglerId=_dx.anglerId;
+            _curr.contestId=_dx.contestId;
+            if(_dx.fish!="Harcsa")
+            {
+                _curr.isNotCatfish=true;
+            }
+            else if(_dx.fish=="Harcsa" && _curr.isCatfish)
+            {
+                if(_curr.sizeOfFish<_dx.sizeOfFish)
+                {
+                    _curr.sizeOfFish=_dx.sizeOfFish;
+                }
+            }
+            else if(_dx.fish=="Harcsa" && !_curr.isCatfish)
+            {
+                _curr.isCatfish=true;
+                _curr.sizeOfFish=_dx.sizeOfFish;
+            }
+        }
+
+    }
+    else
+    {
+        _end = true;
+
+    }
+}
--- assignment_2/main.cpp
+++ assignment_2/main.cpp
@@ -1,63 +1,565 @@
 #include <iostream>
-#include <fstream>
+#include "angler.h"
+#include "contest.h"
 #include <sstream>
 
 using namespace std;
 
-struct Result
+
+angler firstQuestion(string filename)
 {
-    string anglerID;
-    string competetiveID;
-    string fish;
-    int sizeOfFish;
-};
+    Angler t(filename);
+    angler winner;
+    winner.isCatfish=false;
 
-enum Status {abnorm, norm};
+    for(t.first(); !t.end(); t.next())
+    {
+        if(! t.current().isCatfish) ;
+        else if( t.current().isCatfish && winner.isCatfish)
+        {
+            if(winner.sizeOfFish<t.current().sizeOfFish)
+            {
+                winner.anglerId=t.current().anglerId;
+                winner.contestId=t.current().contestId;
+                winner.sizeOfFish=t.current().sizeOfFish;
+            }
+        }
+        else if(t.current().isCatfish && !winner.isCatfish)
+        {
+            winner.anglerId=t.current().anglerId;
+            winner.contestId=t.current().contestId;
+            winner.isCatfish=true;
+            winner.sizeOfFish=t.current().sizeOfFish;
+        }
+    }
+    return winner;
+}
+string secondQuestion(string filename)
+{
+    Contest t(filename);
+    string contestIds;
+    t.first();
+    while(  !t.end())
+    {
+        if(t.current().isCatfish&&t.current().isNotCatfish)
+        {
+            contestIds += t.current().contestId + " ";
 
-bool read(ifstream &f, Result &e, Status &st);
-
+        }
+        t.next();
+    }
+    return contestIds;
+}
+#define NORMAL_MODE
+#ifdef NORMAL_MODE
 int main()
 {
-    ifstream x("inp1.txt");
-    if (x.fail() )
+    try
     {
-        cout << "hibas fajlnev!\n";
-        return 1;
-    }
 
-    Result dx;
-    Status sx;
-    dx.sizeOfFish=0;
-    while(read(x,dx,sx));
-    cout << "The best Fisherman : " << dx.anglerID << " and where the miracle happened:"<< dx.anglerID << endl;
+        string filename = "inp.txt";
+        angler winner;
+        winner = firstQuestion(filename);
+
+        cout << "A legnagyobb harcsa fogó dij nyertese: " << endl;
+        if (!winner.isCatfish)
+        {
+            cout << "Nincs olyan, aki fogott harcsát" << endl;
+
+        }
+        else
+        {
+            cout << winner << endl;
+        }
+        string contestIds = secondQuestion(filename);
+        cout << "Azok a vesenyek, ahol fogtak harcsát is: "<< endl;
+        cout << (contestIds.length()==0?"Nem volt ilyen veseny":contestIds)<<endl;
+    }
+    catch(Angler::error err)
+    {
+        if(err==Angler::FILE_NAME_ERROR)
+        {
+            cout << "Wrong filename!";
+        }
+    }
     return 0;
 }
-
-bool read(ifstream &f, Result &e, Status &st)
+#else
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+TEST_CASE("first task empty file", "inp0.txt")
 {
-    string line;
-    getline(f,line);
-    if (!f.fail() && line!="")
+
+    try
     {
-        st = norm;
-        istringstream in(line);
+        angler winner = firstQuestion("inp0.txt");
+        CHECK_FALSE(winner.isCatfish);
 
-        string anglerID, competetiveID, fish;
-        int sizeOfFish;
-
-        while( in >> anglerID >> competetiveID >> fish >> sizeOfFish )
-        {
-            if(e.sizeOfFish<sizeOfFish)
-            {
-                e.anglerID=anglerID;
-                e.competetiveID=competetiveID;
-                e.fish=fish;
-                e.sizeOfFish=sizeOfFish;
-            }
-        };
     }
-    else
-        st=abnorm;
-
-    return norm==st;
+    catch(Angler::error err)
+    {
+        if(err==Angler::FILE_NAME_ERROR)
+        {
+            cout << "Wrong filename!";
+        }
+    }
 }
+
+TEST_CASE("first task 1 angler", "inp1.txt")
+{
+
+    try
+    {
+        angler winner = firstQuestion("inp1.txt");
+        CHECK (winner.anglerId=="JANIBÁ");
+
+    }
+    catch(Angler::error err)
+    {
+        if(err==Angler::FILE_NAME_ERROR)
+        {
+            cout << "Wrong filename!";
+        }
+    }
+}
+
+TEST_CASE("first task 2 angler", "inp2.txt")
+{
+
+    try
+    {
+        angler winner = firstQuestion("inp2.txt");
+        CHECK (winner.anglerId=="ANNA");
+
+    }
+    catch(Angler::error err)
+    {
+        if(err==Angler::FILE_NAME_ERROR)
+        {
+            cout << "Wrong filename!";
+        }
+    }
+}
+
+TEST_CASE("first task 3 angler", "inp3.txt")
+{
+
+    try
+    {
+        angler winner = firstQuestion("inp3.txt");
+        CHECK (winner.anglerId=="JANIBÁ");
+
+    }
+    catch(Angler::error err)
+    {
+        if(err==Angler::FILE_NAME_ERROR)
+        {
+            cout << "Wrong filename!";
+        }
+    }
+}
+
+TEST_CASE("first task first catch", "inp1.txt")
+{
+
+    try
+    {
+        angler winner = firstQuestion("inp1.txt");
+        CHECK (winner.anglerId=="JANIBÁ");
+
+    }
+    catch(Angler::error err)
+    {
+        if(err==Angler::FILE_NAME_ERROR)
+        {
+            cout << "Wrong filename!";
+        }
+    }
+}
+
+TEST_CASE("first task last catch ", "inp4.txt")
+{
+
+    try
+    {
+        angler winner = firstQuestion("inp1.txt");
+        CHECK (winner.anglerId=="JANIBÁ");
+
+    }
+    catch(Angler::error err)
+    {
+        if(err==Angler::FILE_NAME_ERROR)
+        {
+            cout << "Wrong filename!";
+        }
+    }
+}
+TEST_CASE("first task exists winner ", "inp4.txt")
+{
+
+    try
+    {
+        angler winner = firstQuestion("inp1.txt");
+        CHECK (winner.anglerId=="JANIBÁ");
+
+    }
+    catch(Angler::error err)
+    {
+        if(err==Angler::FILE_NAME_ERROR)
+        {
+            cout << "Wrong filename!";
+        }
+    }
+}
+
+TEST_CASE("first task not exists winner ", "inp5.txt")
+{
+
+    try
+    {
+        angler winner = firstQuestion("inp5.txt");
+        CHECK_FALSE(winner.isCatfish);
+
+    }
+    catch(Angler::error err)
+    {
+        if(err==Angler::FILE_NAME_ERROR)
+        {
+            cout << "Wrong filename!";
+        }
+    }
+}
+TEST_CASE("first task exists more winner ", "inp6.txt")
+{
+
+    try
+    {
+        angler winner = firstQuestion("inp6.txt");
+        CHECK (winner.anglerId=="ANNA");
+
+
+    }
+    catch(Angler::error err)
+    {
+        if(err==Angler::FILE_NAME_ERROR)
+        {
+            cout << "Wrong filename!";
+        }
+    }
+}
+TEST_CASE("first task exists angler with no catch", "inp7.txt")
+{
+
+    try
+    {
+        angler winner = firstQuestion("inp7.txt");
+        CHECK (winner.anglerId=="JANIBÁ");
+
+
+    }
+    catch(Angler::error err)
+    {
+        if(err==Angler::FILE_NAME_ERROR)
+        {
+            cout << "Wrong filename!";
+        }
+    }
+}
+TEST_CASE("first task exists angler with one catch", "inp7.txt")
+{
+
+    try
+    {
+        angler winner = firstQuestion("inp7.txt");
+        CHECK (winner.anglerId=="JANIBÁ");
+
+
+    }
+    catch(Angler::error err)
+    {
+        if(err==Angler::FILE_NAME_ERROR)
+        {
+            cout << "Wrong filename!";
+        }
+    }
+}
+TEST_CASE("first task exists angler with more catch", "inp7.txt")
+{
+
+    try
+    {
+        angler winner = firstQuestion("inp7.txt");
+        CHECK (winner.anglerId=="JANIBÁ");
+
+
+    }
+    catch(Angler::error err)
+    {
+        if(err==Angler::FILE_NAME_ERROR)
+        {
+            cout << "Wrong filename!";
+        }
+    }
+}
+
+TEST_CASE("first task first winner", "inp1.txt")
+{
+
+    try
+    {
+        angler winner = firstQuestion("inp1.txt");
+        CHECK (winner.anglerId=="JANIBÁ");
+
+    }
+    catch(Angler::error err)
+    {
+        if(err==Angler::FILE_NAME_ERROR)
+        {
+            cout << "Wrong filename!";
+        }
+    }
+}
+
+TEST_CASE("first task last winner ", "inp4.txt")
+{
+
+    try
+    {
+        angler winner = firstQuestion("inp1.txt");
+        CHECK (winner.anglerId=="JANIBÁ");
+
+    }
+    catch(Angler::error err)
+    {
+        if(err==Angler::FILE_NAME_ERROR)
+        {
+            cout << "Wrong filename!";
+        }
+    }
+}
+
+TEST_CASE("first task more winner ", "inp8.txt")
+{
+
+    try
+    {
+        angler winner = firstQuestion("inp8.txt");
+        CHECK (winner.anglerId=="ANNA");
+
+    }
+    catch(Angler::error err)
+    {
+        if(err==Angler::FILE_NAME_ERROR)
+        {
+            cout << "Wrong filename!";
+        }
+    }
+}
+
+TEST_CASE("Second task empty file", "inp0.txt")
+{
+
+    try
+    {
+        string contestIds = secondQuestion("inp0.txt");
+        CHECK(0==contestIds.length());
+    }
+    catch(Angler::error err)
+    {
+        if(err==Angler::FILE_NAME_ERROR)
+        {
+            cout << "Wrong filename!";
+        }
+    }
+}
+
+TEST_CASE("Second task 1 contest", "inp1.txt")
+{
+
+    try
+    {
+        string contestIds = secondQuestion("inp1.txt");
+        CHECK ("Kiliti0512 "==contestIds);
+
+    }
+    catch(Angler::error err)
+    {
+        if(err==Angler::FILE_NAME_ERROR)
+        {
+            cout << "Wrong filename!";
+        }
+    }
+}
+TEST_CASE("Second task 2 contest", "inp2.txt")
+{
+
+    try
+    {
+        string contestIds = secondQuestion("inp2.txt");
+        CHECK ("Pilicsi0530 "==contestIds);
+
+    }
+    catch(Angler::error err)
+    {
+        if(err==Angler::FILE_NAME_ERROR)
+        {
+            cout << "Wrong filename!";
+        }
+    }
+}
+TEST_CASE("Second task 3 contest", "inp3.txt")
+{
+
+    try
+    {
+        string contestIds = secondQuestion("inp3.txt");
+        CHECK ("Kiliti0512 "==contestIds);
+
+    }
+    catch(Angler::error err)
+    {
+        if(err==Angler::FILE_NAME_ERROR)
+        {
+            cout << "Wrong filename!";
+        }
+    }
+}
+TEST_CASE("Second task first contest with cat fish", "inp3.txt")
+{
+
+    try
+    {
+        string contestIds = secondQuestion("inp3.txt");
+        CHECK ("Kiliti0512 "==contestIds);
+
+    }
+    catch(Angler::error err)
+    {
+        if(err==Angler::FILE_NAME_ERROR)
+        {
+            cout << "Wrong filename!";
+        }
+    }
+}
+TEST_CASE("Second task last contest with cat fish", "inp4.txt")
+{
+
+    try
+    {
+        string contestIds = secondQuestion("inp4.txt");
+        CHECK ("Kiliti0512 "==contestIds);
+
+    }
+    catch(Angler::error err)
+    {
+        if(err==Angler::FILE_NAME_ERROR)
+        {
+            cout << "Wrong filename!";
+        }
+    }
+}
+TEST_CASE("Second task exists contest with cat fish", "inp4.txt")
+{
+
+    try
+    {
+        string contestIds = secondQuestion("inp4.txt");
+        CHECK ("Kiliti0512 "==contestIds);
+
+    }
+    catch(Angler::error err)
+    {
+        if(err==Angler::FILE_NAME_ERROR)
+        {
+            cout << "Wrong filename!";
+        }
+    }
+}
+TEST_CASE("Second task not exists contest with cat fish", "inp5.txt")
+{
+
+    try
+    {
+        string contestIds = secondQuestion("inp5.txt");
+        CHECK(0==contestIds.length());
+
+    }
+    catch(Angler::error err)
+    {
+        if(err==Angler::FILE_NAME_ERROR)
+        {
+            cout << "Wrong filename!";
+        }
+    }
+}
+TEST_CASE("Second task an angler with no catch", "inp7.txt")
+{
+
+    try
+    {
+        string contestIds = secondQuestion("inp7.txt");
+        CHECK("Kiliti0512 Pilicsi0530 "==contestIds);
+
+    }
+    catch(Angler::error err)
+    {
+        if(err==Angler::FILE_NAME_ERROR)
+        {
+            cout << "Wrong filename!";
+        }
+    }
+}
+TEST_CASE("Second task an angler with one catch", "inp7.txt")
+{
+
+    try
+    {
+        string contestIds = secondQuestion("inp7.txt");
+        CHECK("Kiliti0512 Pilicsi0530 "==contestIds);
+        cout << contestIds << endl;
+
+    }
+    catch(Angler::error err)
+    {
+        if(err==Angler::FILE_NAME_ERROR)
+        {
+            cout << "Wrong filename!";
+        }
+    }
+}
+TEST_CASE("Second task an angler with two catch", "inp7.txt")
+{
+
+    try
+    {
+        string contestIds = secondQuestion("inp7.txt");
+        CHECK("Kiliti0512 Pilicsi0530 "==contestIds);
+        cout << contestIds << endl;
+    }
+    catch(Angler::error err)
+    {
+        if(err==Angler::FILE_NAME_ERROR)
+        {
+            cout << "Wrong filename!";
+        }
+    }
+}
+TEST_CASE("Second task more content", "inp7.txt")
+{
+
+    try
+    {
+        string contestIds = secondQuestion("inp7.txt");
+        CHECK("Kiliti0512 Pilicsi0530 "==contestIds);
+        cout << contestIds << endl;
+    }
+    catch(Angler::error err)
+    {
+        if(err==Angler::FILE_NAME_ERROR)
+        {
+            cout << "Wrong filename!";
+        }
+    }
+}
+#endif
+
