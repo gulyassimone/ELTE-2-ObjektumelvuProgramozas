e52930e4d3d6dfe729eacb10d12b9c8a337f9227
Gulyás Simone <gulyassimone@gmail.com>
2020-05-25 17:27:00 +0200

    m

---
+++ assignment_plus_12_anglerChampionShip/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+class List(string filename) :
+
+int main(int argc, char* argv[])
+{
+    string filename = (argc>1) ? argv[1] : "input.txt";
+    List p(filename);
+
+
+    return 0;
+}
